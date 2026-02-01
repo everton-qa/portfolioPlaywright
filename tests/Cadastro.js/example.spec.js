77e2694602626135a33c89c54543bb3700c88248
Everton <evertton2015@hotmail.com>
2026-02-01 11:20:54 -0300

    refatorar

---
+++ tests/Cadastro.js/example.spec.js
@@ -0,0 +1,50 @@
+import { test, expect } from '@playwright/test';
+
+const {
+    abrirSite2,
+    PreencheNome2,
+    PreencheEmail2,
+    PreencheSenha2,
+    ClicarCadastrar2,
+    validarCadastroSucesso2,
+    validarCadastroFalha2,
+    validarCadastroFalhaNome2,
+} = require('./page-Cadastro')
+
+test.describe('Cadastro de Usuário', () => {
+
+
+    test.beforeEach(async ({ page }) => {
+        await abrirSite2(page)
+    });
+
+
+    test('Cenário 1 - Página de Cadastro', async ({ page }) => {
+
+        await PreencheNome2(page)
+        await PreencheEmail2(page)
+        await PreencheSenha2(page)
+        await ClicarCadastrar2(page)
+        await validarCadastroSucesso2(page)
+        // /\ validar texto por mensagem
+    })
+    test('Cenário 1.1 - Cadastro com sucesso', async ({ page }) => {
+        await cadastroComSucesso2(page)
+    })
+
+    test('Cenário 2 - Cadastro sem e-mail', async ({ page }) => {
+
+        await PreencheNome2(page)
+        await PreencheSenha2(page)
+        await ClicarCadastrar2(page)
+        await validarCadastroFalha2(page)
+    })
+
+    test('Cadastro sem nome', async ({ page }) => {
+
+        await PreencheEmail2(page)
+        await PreencheSenha2(page)
+        await ClicarCadastrar2(page)
+        await validarCadastroFalhaNome2(page)
+    })
+});
---
+++ tests/Cadastro.js/page-Cadastro.js
@@ -0,0 +1,48 @@
+const { expect }= require('@playwright/test');
+
+const abrirSite2 = async (page) => {
+    await page.goto('https://automationpratice.com.br/register')
+}
+
+const PreencheNome2 = async (page) => {
+    await page.locator('#user').fill('eullin do zap')
+}
+const PreencheEmail2 = async (page) => {
+    await page.locator('#email').fill('eullin@hotmail.com')
+}
+const PreencheSenha2 = async (page) => {
+    await page.locator('#password').fill('12345678')
+}
+const ClicarCadastrar2 = async (page) => {
+    await page.getByRole('button', { name: 'Cadastrar' }).click()
+}
+const validarCadastroSucesso2 = async (page) => {
+    await expect(page.getByRole('heading', { name: 'Cadastro realizado!' })).toBeVisible()
+}
+const validarCadastroFalha2 = async (page) => {
+    await expect(page.getByText('O campo e-mail deve ser')).toBeVisible()
+
+}
+const validarCadastroFalhaNome2 = async (page) => {
+    await expect(page.getByText('O campo nome deve ser')).toBeVisible()
+}
+const cadastroComSucesso2 = async (page) => {
+      await PreencheNome2(page)
+        await PreencheEmail2(page)
+        await PreencheSenha2(page)
+        await ClicarCadastrar2(page)
+        await validarCadastroSucesso2(page)
+}
+
+module.exports = {
+    abrirSite2,
+    PreencheNome2,
+    PreencheEmail2,
+    PreencheSenha2,
+    ClicarCadastrar2,
+    validarCadastroSucesso2,
+    validarCadastroFalha2,
+    validarCadastroFalhaNome2,
+    cadastroComSucesso2
+}// 
+
